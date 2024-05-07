5c80a54950dda31849608f80fba57ae78a077a28
Tobias Pfaller <tobias.pfaller@ait.ac.at>
2024-05-07 17:56:20 +0000

    output network dns servers and gateway

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "network_cidr" {
   value = var.cidr
 }
 
+output "network_dns_nameservers" {
+  value = var.dns_nameservers
+}
+
+output "network_gateway" {
+  value = openstack_networking_subnet_v2.subnet.gateway_ip
+}
+
 output "network_id" {
     value = openstack_networking_network_v2.network.id
 }
